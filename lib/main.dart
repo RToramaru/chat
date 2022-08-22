a191cc39307dda107f59c5ed5a69c28cc7b9c2cd
Rafael Almeida <rafael.almeida.soares2012@gmail.com>
2022-08-21 22:02:51 -0300

    configuracao do firebase no projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:chat/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'pages/routes/app_routes.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    name: 'chat-app',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const AppRoute());
 }
